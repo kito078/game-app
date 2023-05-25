7ee528df4ba237663c7f9c20c82c439cecd1d17b
kito078 <101482292+kito078@users.noreply.github.com>
2023-05-25 15:32:40 +0200

    navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import { HStack, Image, Text } from "@chakra-ui/react";
+import React from "react";
+
+function NavBar() {
+  return (
+    <HStack>
+      {/* <Image src={} boxSize='60px/> */}
+      <h3>logo</h3>
+      <Text>Navbar</Text>
+    </HStack>
+  );
+}
+
+export default NavBar;
