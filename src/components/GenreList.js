a8070961bb0997f70435838adf373d919b9b4c1b
kito078 <101482292+kito078@users.noreply.github.com>
2023-05-29 13:21:31 +0200

    genre

--- src/components/GenreList.js
+++ src/components/GenreList.js
@@ -1,15 +1,26 @@
 import React from "react";
 import useGenres from "../hooks/useGenre";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 function GenreList() {
   const { data } = useGenres();
 
   return (
-    <ul>
-      {data.map((data) => (
-        <li>{data.name}</li>
+    <List>
+      {data.map((genre) => (
+        <ListItem paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="10"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 }
 
